eabbf001de5aa7093fdace85bba03fc1e231641c
yoonju977 <yoonju977@gmail.com>
2024-11-11 22:51:38 +0900

    ✨Feat:Stack model & UserStack model

--- api/stacks/models.py
+++ api/stacks/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from users.models import User
 
-# Create your models here.
+
+class Stack(models.Model):
+    name = models.CharField(max_length=255, null=False)
+
+    def __str__(self):
+        return self.name
+
+
+class UserStack(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="user_stacks")
+    stack = models.ForeignKey(
+        Stack, on_delete=models.CASCADE, related_name="stack_users"
+    )
+
+    def __str__(self):
+        return f"{self.user.username} - {self.stack.name}"
