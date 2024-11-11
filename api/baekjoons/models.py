db9b873363be8398e08d4f1d93e7897c8bf90c30
dayeonkimm <ekdyd516@gmail.com>
2024-11-11 17:00:44 +0900

    ✨Feat: 백준 api

--- api/baekjoons/models.py
+++ api/baekjoons/models.py
@@ -1,3 +1,14 @@
+from common.models import TimeStampModel
 from django.db import models
+from users.models import User
 
-# Create your models here.
+
+class Baekjoon(TimeStampModel):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    solved_problem = models.IntegerField()
+    score = models.IntegerField()
+    tier = models.CharField(max_length=20)
+    date = models.DateField()
+
+    def __str__(self):
+        return f"{self.date}-{self.score}"
