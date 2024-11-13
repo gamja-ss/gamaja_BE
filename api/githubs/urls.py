d7f34743b04aaf5cba11717b837907cae8ddef8a
dayeonkimm <ekdyd516@gmail.com>
2024-11-13 15:17:39 +0900

    🔨Refactor: api 세분화

--- api/githubs/urls.py
+++ api/githubs/urls.py
@@ -1,7 +1,20 @@
 from django.urls import path
-from githubs.views import GetTodayGithubCommits, UpdateGithubCommits
+from githubs.views import (
+    GetDateGithubCommits,
+    GetPeriodGithubCommits,
+    GetTodayGithubCommits,
+    GetTotalGithubCommits,
+    UpdateGithubCommits,
+)
 
 urlpatterns = [
     path("update/", UpdateGithubCommits.as_view(), name="update_github_commits"),
+    path("total/", GetTotalGithubCommits.as_view(), name="get_total_github_commits"),
     path("today/", GetTodayGithubCommits.as_view(), name="get_today_github_commits"),
+    path(
+        "date/",
+        GetDateGithubCommits.as_view(),
+        name="get_date_github_commits",
+    ),
+    path("period/", GetPeriodGithubCommits.as_view(), name="get_period_github_commits"),
 ]
