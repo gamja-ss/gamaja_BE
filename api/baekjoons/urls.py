06c0a675d7a46fc65b25f90f49c9f6fddf3b65cc
dayeonkimm <ekdyd516@gmail.com>
2024-11-15 15:10:41 +0900

    ✨Feat: 백준 정보 계산 api

--- api/baekjoons/urls.py
+++ api/baekjoons/urls.py
@@ -1,8 +1,37 @@
 from django.urls import path
 
-from .views import GetTodayBaekjoon, UpdateBaekjoon
+from .views import (
+    GetDateBaekjoonScore,
+    GetDateBaekjoonSP,
+    GetPeriodBaekjoonScore,
+    GetPeriodBaekjoonSP,
+    GetTodayBaekjoonScore,
+    GetTodayBaekjoonSP,
+    GetTotalBaekjoonInfo,
+    UpdateBaekjoonInfo,
+)
 
 urlpatterns = [
-    path("update/", UpdateBaekjoon.as_view(), name="update_baekjoon"),
-    path("today/", GetTodayBaekjoon.as_view(), name="get_today_baekjoon"),
+    path("update/", UpdateBaekjoonInfo.as_view(), name="update_baekjoon_info"),
+    path("total/", GetTotalBaekjoonInfo.as_view(), name="get_total_baekjoon_info"),
+    path("today/sp/", GetTodayBaekjoonSP.as_view(), name="get_today_baekjoon_sp"),
+    path(
+        "today/score/", GetTodayBaekjoonScore.as_view(), name="get_today_baekjoon_score"
+    ),
+    path(
+        "date/sp/",
+        GetDateBaekjoonSP.as_view(),
+        name="get_date_baekjoon_sp",
+    ),
+    path(
+        "date/score/",
+        GetDateBaekjoonScore.as_view(),
+        name="get_date_baekjoon_score",
+    ),
+    path("period/sp/", GetPeriodBaekjoonSP.as_view(), name="get_period_baekjoon_sp"),
+    path(
+        "period/score/",
+        GetPeriodBaekjoonScore.as_view(),
+        name="get_period_baekjoon_score",
+    ),
 ]
