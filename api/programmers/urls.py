9e29972a1bf5b524d66c6c4bef30e4d1701e1136
dayeonkimm <ekdyd516@gmail.com>
2024-11-15 15:11:05 +0900

    ✨Feat: 프로그래머스 정보 계산 api

--- api/programmers/urls.py
+++ api/programmers/urls.py
@@ -1,8 +1,45 @@
 from django.urls import path
 
-from .views import GetTodayProgrammers, UpdateProgrammers
+from .views import (
+    GetDateProgrammersScore,
+    GetDateProgrammersST,
+    GetPeriodProgrammersScore,
+    GetPeriodProgrammersST,
+    GetTodayProgrammersScore,
+    GetTodayProgrammersST,
+    GetTotalProgrammersInfo,
+    UpdateProgrammersInfo,
+)
 
 urlpatterns = [
-    path("update/", UpdateProgrammers.as_view(), name="update_programmers"),
-    path("today/", GetTodayProgrammers.as_view(), name="get_today_programmers"),
+    path("update/", UpdateProgrammersInfo.as_view(), name="update_programmers_info"),
+    path(
+        "total/", GetTotalProgrammersInfo.as_view(), name="get_total_programmers_info"
+    ),
+    path(
+        "today/st/", GetTodayProgrammersST.as_view(), name="get_today_programmersn_st"
+    ),
+    path(
+        "today/score/",
+        GetTodayProgrammersScore.as_view(),
+        name="get_today_programmers_score",
+    ),
+    path(
+        "date/st/",
+        GetDateProgrammersST.as_view(),
+        name="get_date_programmers_st",
+    ),
+    path(
+        "date/score/",
+        GetDateProgrammersScore.as_view(),
+        name="get_date_programmers_score",
+    ),
+    path(
+        "period/st/", GetPeriodProgrammersST.as_view(), name="get_period_programmers_st"
+    ),
+    path(
+        "period/score/",
+        GetPeriodProgrammersScore.as_view(),
+        name="get_period_programmers_score",
+    ),
 ]
