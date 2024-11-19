685af4ddbf9e97c571ae3980713eba5e577d0087
dayeonkimm <ekdyd516@gmail.com>
2024-11-19 22:03:23 +0900

    ✨Feat: 팔로우 관련 모델 생성

--- api/users/models.py
+++ api/users/models.py
@@ -65,6 +65,8 @@ class User(AbstractBaseUser, PermissionsMixin, TimeStampModel):
     email = models.EmailField(max_length=255, null=True)
     profile_url = models.CharField(max_length=255, null=True)
     nickname = models.CharField(max_length=255, null=False)
+    followers_count = models.PositiveIntegerField(default=0)
+    following_count = models.PositiveIntegerField(default=0)
 
     # 계정 관련 필드
     github_id = models.CharField(max_length=255, null=True)
