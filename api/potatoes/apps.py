e2a4734fb4a0325e9e076bb48692e9d9d93e53ff
yoonju977 <yoonju977@gmail.com>
2024-11-22 13:53:44 +0900

    ✨feat : Add Potato Service: User Potato and Preset Features

--- api/potatoes/apps.py
+++ api/potatoes/apps.py
@@ -2,5 +2,8 @@ from django.apps import AppConfig
 
 
 class PotatoesConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'potatoes'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "potatoes"
+
+    def ready(self):
+        import potatoes.signals
