8479ba7e5686ec5ed572a218446ba6885ab0eeb5
dayeonkimm <ekdyd516@gmail.com>
2024-11-24 13:28:39 +0900

    🔨Refactor: 방명록 닉네임 시리얼라이저 수정

--- api/guestbooks/serializers.py
+++ api/guestbooks/serializers.py
@@ -7,15 +7,9 @@ from .models import Guestbook
 
 
 class GuestbookSerializer(serializers.ModelSerializer):
-    guest_nickname = serializers.SerializerMethodField()
+    guest_nickname = serializers.CharField(source="guest.nickname", read_only=True)
 
     class Meta:
         model = Guestbook
         fields = ["id", "host", "guest_nickname", "content", "created_at", "updated_at"]
         read_only_fields = ["id", "guest_nickname", "created_at", "updated_at"]
-
-    def get_guest_nickname(self, obj) -> Optional[str]:
-        try:
-            return obj.guest.nickname
-        except User.DoesNotExist:
-            return None
