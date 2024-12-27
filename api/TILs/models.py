fca706c4ca8a72d0e588883f10d4c9d9b741f551
dayeonkimm <ekdyd516@gmail.com>
2024-12-27 23:59:02 +0900

    ✨Feat: TIL 이미지 모델

--- api/TILs/models.py
+++ api/TILs/models.py
@@ -7,3 +7,18 @@ class TIL(TimeStampModel):
     user = models.ForeignKey(User, on_delete=models.CASCADE, null=False)
     title = models.CharField(max_length=250, null=False)
     content = models.TextField(null=False)
+
+
+class TILImage(TimeStampModel):
+    TIL = models.ForeignKey(
+        TIL, on_delete=models.CASCADE, null=True, related_name="images"
+    )
+    image = models.URLField(max_length=500, null=False)  # S3 URL을 직접 저장
+    is_temporary = models.BooleanField(default=True)
+
+    def __str__(self):
+        return f"{self.TIL.title} - {self.id}"
+
+    @property
+    def image_url(self):
+        return self.image if self.image else None  # 이미지를 반환할 때는 S3 URL 반환
