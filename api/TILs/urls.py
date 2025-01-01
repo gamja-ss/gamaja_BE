2ad136e4c517f647bb6dfd3a6e63c77b335fd9ac
dayeonkimm <ekdyd516@gmail.com>
2025-01-01 23:56:10 +0900

    ✨Feat: 엔드포인트 등록

--- api/TILs/urls.py
+++ api/TILs/urls.py
@@ -1,8 +1,16 @@
 from django.urls import path
 
-from .views import CreateTILAPI, DeleteTILAPI, UpdateTILAPI
+from .views import (
+    CreateTILAPI,
+    DeleteTempImageAPI,
+    DeleteTILAPI,
+    UpdateTILAPI,
+    UploadTempImageAPI,
+)
 
 urlpatterns = [
+    path("upload/image/", UploadTempImageAPI.as_view(), name="upload_temp_image"),
+    path("delete/image", DeleteTempImageAPI.as_view(), name="delete_temp_image"),
     path("create/", CreateTILAPI.as_view(), name="create-til"),
     path("update/<int:pk>/", UpdateTILAPI.as_view(), name="update-til"),
     path("delete/<int:pk>/", DeleteTILAPI.as_view(), name="delete-til"),
